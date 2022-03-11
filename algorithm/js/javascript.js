96445d2c2eff97739c697742a8dfac687ba7aa6b
art11010 <49858735+art11010@users.noreply.github.com>
2022-03-11 21:11:31 +0900

    front

---
+++ algorithm/js/javascript.js
@@ -0,0 +1,10 @@
+// console.log('hello world');
+
+// const fs = require('fs');
+// const input = fs.readFileSync('./input.txt', 'utf8');
+// console.log(input);
+
+let 안녕 = '하세요';
+let hi = 'hello';
+let HI = 'HELLO';
+console.log(안녕, hi, HI)
