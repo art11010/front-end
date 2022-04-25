2ac6ebebb7df3a71b604ebcdb398b47d585772a8
art11010 <49858735+art11010@users.noreply.github.com>
2022-04-26 01:21:17 +0900

    Update linear.js

--- algorithm/js/linear.js
+++ algorithm/js/linear.js
@@ -810,9 +810,9 @@ LinkedList.prototype.removeIdx = function(value){
 // 22. 이중 연결 리스트 구현하기(1), (2)
 // Node() : data와 point를 가지고 있는 객체
 function DoubleNode(data){
-	this.prev = null;
 	this.data = data;
 	this.next = null;
+	this.prev = null;
 }
 // DoubleLinkedList() : head와 length를 가지고 있는 객체
 function DoubleLinkedList(){
@@ -820,19 +820,192 @@ function DoubleLinkedList(){
 	this.tail = null;
 	this.length = 0;
 }
+// (1)
 // size() : 연결 리스트 내 노드 개수 확인
 DoubleLinkedList.prototype.size = function(){
 	return this.length;
 }
 // isEmpty() : 객체 내 노드 존재 여부 파악
 DoubleLinkedList.prototype.isEmpty = function(){
-	return this.length == 0;
+	return this.length === 0;
 }
+
+// Test code
+// let dll = new DoubleLinkedList();
+// let node = new DoubleNode();
+// console.log(dll);
+
+// dll.head = node;
+// dll.tail = node;
+// dll.length++;
+// console.log(dll);
+
+// node = new Node(456);
+// dll.tail.next = node;
+// node.prev = dll.tail;
+// dll.tail = node;
+// dll.length++;
+// console.log(dll);
+
+// (2)
+// printNode() : 노드 정방향 출력
+DoubleLinkedList.prototype.printNode = function(){
+	process.stdout.write('head -> ');
+	for(let node = this.head; node != null; node = node.next){
+		process.stdout.write(`${node.data} -> `);
+	}
+	console.log('null')
+}
+
+// printNodeInverse() : 노트 역방향 출력
+DoubleLinkedList.prototype.printNodeInverse = function(){
+	let temp = [];
+	process.stdout.write('null <- ');
+	for(let node = this.tail; node != null; node = node.prev){
+		temp.push(node.data);
+	}
+	for(let i = temp.length - 1; i >= 0; i--){
+		process.stdout.write(`${temp[i]} <- `);
+	}
+	console.log('tail');
+}
+
+// append() : 연결 리스트 가장 끝에 노드 추가
+DoubleLinkedList.prototype.append = function(value){
+	let node = new DoubleNode(value);
+	if(this.head === null){
+		this.head = node;
+		this.tail = node;
+	}else{
+		this.tail.next = node;
+		node.prev = this.tail;
+		this.tail = node;
+	}
+	this.length++;
+}
+
+// Test code
+// let dll = new DoubleLinkedList();
+// dll.append(1);
+// console.log(dll);
+// dll.append(10);
+// dll.append(100);
+
+// dll.printNode();
+// dll.printNodeInverse();
 
 // - - - - - - - - - - - - - - - - - - - -
 
-// 23. 이중 연결 리스트 구현하기(2), (3)
+// 23. 이중 연결 리스트 구현하기(3), (4)
+// (3)
+// insert() : position 위치에 노드 추가
+DoubleLinkedList.prototype.insert = function(value, position = 0){
+	if(position < 0 || position > this.length){
+		return false;
+	}
+	let node = new DoubleNode(value),
+		current = this.head,
+		index = 0,
+		prev;
+	if(position === 0){
+		if(this.head === null){
+			this.head = node;
+			this.tail = node;
+		}else{
+			node.next = current;
+			current.prev = node;
+			this.head = node;
+		}
+	}else if(position === this.length){
+		current = this.tail;
+		current.next = node;
+		node.prev = current;
+		this.tail = node;
+	}else{
+		while(index++ < position){
+			prev = current;
+			current = current.next;
+		}
+		node.next = current;
+		prev.next = node;
 
+		current.prev = node;
+		node.prev = prev;
+	}
+	this.length++;
+	return true;
+}
+
+// Test code
+// let dll = new DoubleLinkedList();
+// dll.insert(1);
+// dll.insert(10);
+// dll.insert(100);
+// dll.printNode();
+// dll.printNodeInverse();
+
+// dll.insert(2, 1);
+// dll.insert(3, 3);
+// dll.printNode();
+// dll.printNodeInverse();
+
+// (4)
+// remove() : value 데이터를 찾아 노드 삭제
+DoubleLinkedList.prototype.remove = function(value){
+	let current = this.head,
+		prev = current;
+	while(current.data != value && current.next != null){
+		prev = current;
+		current = current.next;
+	}
+
+	if(current.data != value){
+		return null;
+	}
+
+	if(current === this.head){
+		this.head = current.next;
+		if(this.length === 1) {this.tail = null; console.log('hi')}
+		else this.head.prev = null;
+	}else if(current === this.tail){
+		this.tail = current.prev;
+		this.tail.next = null;
+	}else{
+		prev.next = current.next;
+		current.next.prev = prev;
+	}
+
+	this.length--;
+
+	return current.data;
+}
+
+// Test code
+let dll = new DoubleLinkedList();
+let node = new DoubleNode();
+console.log(dll)
+console.log(node)
+// dll.insert(1);
+// dll.insert(10);
+dll.insert(100);
+// dll.insert(2, 1);
+// dll.insert(3, 3);
+dll.printNode();
+dll.printNodeInverse();
+
+// console.log(dll.remove(1000));
+// dll.printNode();
+// dll.printNodeInverse();
+// console.log(dll.remove(1));
+// dll.printNode();
+// dll.printNodeInverse();
+// console.log(dll.remove(2));
+// dll.printNode();
+// dll.printNodeInverse();
+console.log(dll.remove(100));
+console.log(dll.size());
+dll.printNode();
+dll.printNodeInverse();
 
 // - - - - - - - - - - - - - - - - - - - -
 
