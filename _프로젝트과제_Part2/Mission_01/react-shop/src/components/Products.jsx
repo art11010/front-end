4935df5efcbe3313dd5f7b629fa4f373c18b6733
art11010 <49858735+art11010@users.noreply.github.com>
2022-08-19 00:59:02 +0900

    mission

--- _프로젝트과제_Part2/Mission_01/react-shop/src/components/Products.jsx
+++ _프로젝트과제_Part2/Mission_01/react-shop/src/components/Products.jsx
@@ -5,12 +5,14 @@ function Products(props) {
 	// console.log(props.product)
 
 	return (
-		<>
-			<section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
-				<h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">패션</h2>
-				<div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list" data-scroll="true">
-					{ info.map((doc) =>
-						<a key={ doc.id } className="card card-bordered border-gray-200 dark:border-gray-800 card-compact lg:card-normal">
+		<section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
+			{ info.map((doc) =>
+				<React.Fragment key={ doc.id } >
+					{ doc.id % 4 == 1 && 
+						<h2 className="mb-5 lg:mb-8 text-3xl lg:text-4xl text-center font-bold">{doc.category}</h2>
+					}
+					<div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4 item_list" data-scroll="true">
+						<a className="card card-bordered border-gray-200 dark:border-gray-800 card-compact lg:card-normal">
 							<figure className="flex h-80 bg-white overflow-hidden">
 								<img src={ doc.image } alt="상품 이미지" className="transition-transform duration-300" />
 							</figure>
@@ -19,10 +21,10 @@ function Products(props) {
 								<p className="text-base">${ doc.price }</p>
 							</div>
 						</a>
-						) }
 					</div>
-				</section>
-		</>
+				</React.Fragment>
+			) }
+		</section>
 	);
 }
 
--- _프로젝트과제_Part2/Mission_01/react-shop/src/components/Slide.jsx
+++ _프로젝트과제_Part2/Mission_01/react-shop/src/components/Slide.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-// import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
 function Slide(props) {
 	return (
 		<div>
-			 <Carousel>
+			 <Carousel autoPlay showThumbs={false} >
 					<div>
 						<img src="https://react-shop-oinochoe.vercel.app/img_shop_fashion.jpeg" />
-						<p className="legend">Legend 1</p>
+						<div className="carousel-description absolute left-auto right-auto bottom-1/3 mb-10 text-left w-full lg:container px-4 md:px-10"><h2 className="text-2xl lg:text-4xl font-bold text-white">물빠진 청바지!</h2><p className="my-2 text-white">이제 막 도착한 패션 청바지를 구경해 보세요.</p><a className="btn btn-sm lg:btn-md mt-3" href="/fashion">바로가기<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></a></div>
 					</div>
 					<div>
 						<img src="https://react-shop-oinochoe.vercel.app/img_shop_digital.jpeg" />
-						<p className="legend">Legend 1</p>
+						<div className="carousel-description absolute left-auto right-auto bottom-1/3 mb-10 text-left w-full lg:container px-4 md:px-10"><h2 className="text-2xl lg:text-4xl font-bold text-white">신속한 업무처리!</h2><p className="my-2 text-white">다양한 디지털 상품을 둘러보세요.</p><a className="btn btn-sm lg:btn-md mt-3" href="/digital">바로가기<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></a></div>
 					</div>
 					<div>
 						<img src="https://react-shop-oinochoe.vercel.app/img_shop_grocery.jpeg" />
-						<p className="legend">Legend 1</p>
+						<div className="carousel-description absolute left-auto right-auto bottom-1/3 mb-10 text-left w-full lg:container px-4 md:px-10"><h2 className="text-2xl lg:text-4xl font-bold text-white">신선한 식품!</h2><p className="my-2 text-white">농장 직배송으로 더욱 신선한 식료품을 만나보세요.</p><a className="btn btn-sm lg:btn-md mt-3" href="/grocery">바로가기<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></a></div>
 					</div>
 			</Carousel>
 		</div>
@@ -26,31 +24,3 @@ function Slide(props) {
 }
 
 export default Slide;
-
-// import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
-// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from 'react-responsive-carousel';
-
-// class DemoCarousel extends Slide {
-//     render() {
-//         return (
-//             <Carousel>
-//                 <div>
-// 									<img src="image/img_shop_fashion.jpeg" />
-// 									<p className="legend">Legend 1</p>
-//                 </div>
-//                 <div>
-// 									<img src="image/img_shop_digital.jpeg" />
-// 									<p className="legend">Legend 1</p>
-//                 </div>
-//                 <div>
-// 									<img src="image/img_shop_grocery.jpeg" />
-// 									<p className="legend">Legend 1</p>
-//                 </div>
-//             </Carousel>
-//         );
-//     }
-// });
-
-// ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
