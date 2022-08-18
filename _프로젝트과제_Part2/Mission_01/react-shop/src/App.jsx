38b3b3dd49be7b41c7bfc675a7ff97082bac56cc
art11010 <49858735+art11010@users.noreply.github.com>
2022-08-19 03:00:09 +0900

    mission

--- _프로젝트과제_Part2/Mission_01/react-shop/src/App.jsx
+++ _프로젝트과제_Part2/Mission_01/react-shop/src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import './App.css'
 import Nav from './components/Nav';
 import Slide from './components/Slide';
 import Products from './components/Products';
