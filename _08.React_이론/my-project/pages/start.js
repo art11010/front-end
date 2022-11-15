d9da9b62c48091fad390545a77d29df28f015b85
art11010 <49858735+art11010@users.noreply.github.com>
2022-11-16 00:10:41 +0900

    react

--- _08.React_이론/my-project/pages/start.js
+++ _08.React_이론/my-project/pages/start.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchUser } from "../src/modules/api";
 
 export default function start() {
+  const [name, setName] = useState("제로베이스");
+
+  useEffect(() => {
+    async function fetch() {
+      const res = await fetchUser();
+      setName(res.name);
+    }
+    fetch();
+  }, [])
+
   return (
-    <div>
-      제로베이스님, 입사를 축하드립니다! 원하시는 개발 장비를 선택해주세요.
+    <div className="container">
+      <div className="wrapper">
+        <h3 className="title">
+          { name }님, 입사를 축하드립니다! 원하시는 개발 장비를 선택해주세요.
+        </h3>
+      </div>
     </div>
   );
 }
